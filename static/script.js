86885757ad4c2f1f166a3cb1ee0803ab37ab4a86
AIDAUM33\USER <skyjjw37@gmail.com>
2025-04-12 15:06:59 +0900

    홍길동, 김영희 이름 수정

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
 // 예시 데이터 (실제 사용 시 여기에 모든 학생 정보를 추가)
 const studentAccounts = [
-  { id: '20230123', name: '홍길동', googleId: 'hong123@school.com', password: 'abcd1234' },
-  { id: '20230124', name: '김영희', googleId: 'kimyh@school.com', password: 'pass2023' },
-  { id: '20230125', name: '이철수', googleId: 'lee25@school.com', password: 'pw4567' }
+  { id: '20230123', name: '홍수정', googleId: 'hong123@school.com', password: 'abcd1234' },
+  { id: '20230124', name: '김명희', googleId: 'kimyh@school.com', password: 'pass2023' },
+  { id: '20230125', name: '이철민', googleId: 'lee25@school.com', password: 'pw4567' }
 ];
 
 document.getElementById('search-form').addEventListener('submit', function (e) {
